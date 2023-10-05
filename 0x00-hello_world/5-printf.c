76f6edab092510c5c1d3f5c114b806d68d430cbf
charif <mohammedcharifj@gmail.com>
2023-10-05 07:13:21 -0700

    updated

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  *
+  * Decription: c program that prints with printf function
+  *
+  * Return: success - 0
+  *
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
