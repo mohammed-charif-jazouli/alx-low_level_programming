98a29fd370ef0bebb5e8c9f25dd075b16f432e2a
charif <mohammedcharifj@gmail.com>
2023-10-05 08:22:26 -0700

    update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  *
+  * Decription: c program that prints the size of various types
+  *
+  * Return: success - 1
+  *
+  */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
