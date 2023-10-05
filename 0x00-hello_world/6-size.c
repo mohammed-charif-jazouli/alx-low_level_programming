bc73ca67949a9f0ba88b06988cf1989f6070b78a
charif <mohammedcharifj@gmail.com>
2023-10-05 08:10:09 -0700

    update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  *
+  * Decription: c program that prints with printf function
+  *
+  * Return: success - 0
+  *
+  */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(unsigned int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(double));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
