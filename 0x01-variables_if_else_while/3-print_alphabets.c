1f125e7b1255688ff9e3928f4b1dc7cd9e7835e1
charif <mohammedcharifj@gmail.com>
2023-10-06 12:47:38 -0700

    updated

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,13 @@
 
 int main(void)
 {
-	char a = 'a', new_line = '\n';
+	char a = 'a', A = 'A', new_line = '\n';
 	int i;
 
 	for (i = 0; i < 26; i++)
 		putchar(a + i);
+	for (i = 0; i < 26; i++)
+		putchar(A + i);
 	putchar(new_line);
 
 	return (0);
