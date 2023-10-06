6de15324b6f1796fd40fd963f533b0aecb522e16
charif <mohammedcharifj@gmail.com>
2023-10-06 13:25:13 -0700

    updated

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: c file that prints the alphabet
+ *
+ * Return: success - 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
