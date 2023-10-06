085d3f14600e5f6bc5471c83350f4cd035fcec62
charif <mohammedcharifj@gmail.com>
2023-10-06 12:57:32 -0700

    updated

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: c file that prints the alphabet (no e and q)
+ *
+ * Return: success - 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf(i);
+	printf("\n");
+
+	return (0);
+}
