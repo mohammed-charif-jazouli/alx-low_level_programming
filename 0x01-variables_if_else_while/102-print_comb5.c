0837178d29ba33ed7180bcec454725bfeaa214c5
charif <mohammedcharifj@gmail.com>
2023-10-06 20:05:01 -0700

    updated

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: c file that prints the alphabet
+ *
+ * Return: success - 0
+ *
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar('0' + (i / 10));
+			putchar('0' + (i % 10));
+			putchar(' ');
+			putchar('0' + (j / 10));
+			putchar('0' + (j % 10));
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
