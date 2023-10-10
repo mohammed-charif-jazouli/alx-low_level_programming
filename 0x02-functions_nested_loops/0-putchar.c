2d061a871a5ef898599ca1c3f0488ffd55d249ea
charif <mohammedcharifj@gmail.com>
2023-10-10 08:18:37 -0700

    updated

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: c file that prints the alphabet
+ *
+ * Return: success - 0
+ *
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
