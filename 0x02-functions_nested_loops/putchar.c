66dfeae0e011ec4fcb9623ac07b7bf0579e8ee5f
charif <mohammedcharifj@gmail.com>
2023-10-10 16:30:36 -0700

    updated

---
+++ 0x02-functions_nested_loops/putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: c file that prints the alphabet
+ *
+ * Return: success - 0
+ *
+ */
+
+int _putchar(char c)
+{
+	return (0);
+}
