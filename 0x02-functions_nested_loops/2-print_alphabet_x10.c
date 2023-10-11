651839b51e5123471dc3618fa2947ab52d222bd0
charif <mohammedcharifj@gmail.com>
2023-10-11 02:24:28 +0100

    updated

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet
+ *
+ * Return: void
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+}
